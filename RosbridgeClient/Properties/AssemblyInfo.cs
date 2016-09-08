0fab9240b1c2dfdb1a15ce96b01d75a1128904d7
Jonas Follesø <jonas@follesoe.no>
2016-09-08 13:14:33 +0200

    Rename rosbrige_client_common to RosbridgeCommon

--- RosbridgeClient/Properties/AssemblyInfo.cs
+++ RosbridgeClient/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("rosbridge_client_common")]
+[assembly: AssemblyTitle("RosbridgeClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("rosbridge_client_common")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("RosbridgeClient")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
